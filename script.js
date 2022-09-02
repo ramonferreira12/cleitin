db4ef61928613473d386b615bf1e198f6244bbf5
ramonferreira12 <112702960+ramonferreira12@users.noreply.github.com>
2022-09-02 10:22:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function calcSum() {
+    let num1 = document.getElementsByName("num1")[0].value;
+    let num2 = document.getElementsByName("num2")[0].value;
+    let sum = (Number(num1) + Number(num2));
+    document.getElementsByName("sum")[0].value = sum;
+}
+
+function sub() {
+    let num1 = document.getElementsByName("num1")[0].value;
+    let num2 = document.getElementsByName("num2")[0].value;
+    let sum = (Number(num1) - Number(num2));
+    document.getElementsByName("sum")[0].value = sum;
+}
+function div() {
+    let num1 = document.getElementsByName("num1")[0].value;
+    let num2 = document.getElementsByName("num2")[0].value;
+    let sum = (Number(num1)/Number(num2));
+    document.getElementsByName("sum")[0].value = sum;
+}
+function multi() {
+    let num1 = document.getElementsByName("num1")[0].value;
+    let num2 = document.getElementsByName("num2")[0].value;
+    let sum = (Number(num1)*Number(num2));
+    document.getElementsByName("sum")[0].value = sum;
+}
